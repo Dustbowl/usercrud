5e13930e657dab0c9b9a4ea23ca91361bde381f5
Dustbowl <vuonganthony@hotmail.com>
2024-09-27 13:21:54 -0500

    working deployment

--- src/main/java/com/example/security/SpringSecurityConfiguration.java
+++ src/main/java/com/example/security/SpringSecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 public class SpringSecurityConfiguration {
 	
 	@Bean
--- src/main/java/security/SpringSecurityConfiguration.java
+++ src/main/java/security/SpringSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,14 +9,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 // This package is not accessed because its outside the package of Main class
 
-@Configuration
-@EnableWebSecurity
 public class SpringSecurityConfiguration {
-	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		
+		http.csrf().disable();
+		
+		http.httpBasic(withDefaults());
+
 		return http.build();
+		
 	}
 
 }
